dfb53a61e48a71ac39fee65dc671666612bf0337
Djair Guilherme <djair.jr@gmail.com>
2025-01-16 15:33:19 -0300

    Update SFX_Dalila_Cascavel.ino
    Atualizando o código a partir do código original do Luciano Moutinho

--- SFX_Dalila_Cascavel.ino
+++ SFX_Dalila_Cascavel.ino
@@ -1,53 +1,54 @@
+
+
 #define PIRSENSOR 2
 #define CHOCALHO 3
-#define SHAKETIME 250
 
-int state = LOW;
-int val = 0;
+int unused[14] = {4, 5, 6, 7, 8, 9, 10, 11, 12, A1, A2, A3, A4, A5};
 
 void setup() {
   Serial.begin (115200);
   delay (1000);
   Serial.println ("+++ Cascavel Cabaça +++");
+
+  // Gerador de Aleatórios
   randomSeed(analogRead(0));
+  
   pinMode (CHOCALHO, OUTPUT);
   pinMode (LED_BUILTIN, OUTPUT);
+
+  // Led aceso in LOW
+  digitalWrite (LED_BUILTIN, HIGH);
+
+  // Chocalho inicia desligado
+  digitalWrite (CHOCALHO, LOW);
+  
   pinMode (PIRSENSOR, INPUT);
+  
+  // Evitando que os pinos fiquem flutuantes.
+  for (int i = 0; i < 14; i++) {
+    pinMode (unused[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
+  int = pirState = digitalRead (PIRSENSOR);
 
-  val = digitalRead(PIRSENSOR);   // read sensor value
-  if (val == HIGH) {           // check if the sensor is HIGH
-    digitalWrite(LED_BUILTIN, HIGH);   // turn LED ON
-    delay(100);                // delay 100 milliseconds
-
-    if (state == LOW) {
-      Serial.println("Movimento Detectado");
-      state = HIGH;
-      cascavel();
+  if (pirState == HIGH) {
+    digitalWrite (LED_BUILTIN, LOW);
+    int numPulses = random (4,8);
+    for (int i = 0; i< numPulses; i++) {
+      digitalWrite (CHOCALHO, HIGH);
+      
+      // Esses delays foram importados do código original do Luciano
+      delay(90);
+      digitalWrite (CHOCALHO, LOW);
+      delay (110);  
     }
+   
+    int waitTime = random (5,8);
+    delay (waitTime * 1000);
   }
-  else {
-    digitalWrite(LED_BUILTIN, LOW); // turn LED OFF
-    delay(200);             // delay 200 milliseconds
-
-    if (state == HIGH) {
-      Serial.println("Sem Movimento");
-      state = LOW;
-    }
-  }
+  // Apaga o Led
+  digitalWrite (LED_BUILTIN, HIGH);
 }
 
-
-void cascavel() {
-  int interval = random(3, 6);
-  
-  for (int i = 0; i < interval; i++) {
-    Serial.println (">> Chacoalha <<");
-    digitalWrite (CHOCALHO, HIGH);
-    delay (SHAKETIME);
-    digitalWrite (CHOCALHO, LOW);
-    delay (SHAKETIME);
-  }
-}
